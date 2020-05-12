6c3a40499796d8e755dc7e6dc9c12a055fcef484
Lalu Ferdian Yusuf <64566098+laluferdianyusuf@users.noreply.github.com>
2020-05-12 17:11:13 +0800

    Create jobsheet 2 C.c
    Mengisi dan menampilkan elemen array index berdasarkan NIM

---
+++ jobsheet 2 C.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+	printf ("Nama     : Lalu Ferdian Yusuf\n");
+	printf ("Nim      : F1B019077\n");
+	printf ("Kelompok : 16\n");
+	
+	int x [7];
+	
+	printf("Masukkan nilai 1 :");
+	scanf("%d",x);
+	
+	printf("Masukkan nilai 2 :");
+	scanf("%d",x);
+	
+	printf("Masukkan nilai 3 :");
+	scanf("%d",x);
+	
+	printf("Masukkan nilai 4 :");
+	scanf("%d",x);
+	
+	printf("Masukkan nilai 5 :");
+	scanf("%d",x);
+	
+	printf("Masukkan nilai 6 :");
+	scanf("%d",x);
+	
+	printf("Masukkan nilai 7 :");
+	scanf("%d",x);
+
+}
