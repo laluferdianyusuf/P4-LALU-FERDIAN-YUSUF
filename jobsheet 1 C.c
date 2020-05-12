8717aa5b2450ff4fce1d0dac76d2c86038b0b0b9
Lalu Ferdian Yusuf <64566098+laluferdianyusuf@users.noreply.github.com>
2020-05-12 17:05:30 +0800

    Create jobsheet 1 C.c
    program array satu dimensi menampilkan NIM

---
+++ jobsheet 1 C.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+	printf ("Nama     : Lalu Ferdian Yusuf\n");
+	printf ("Nim      : F1B019077\n");
+	printf ("Kelompok : 16\n");
+	
+	int nim[7];
+	
+	nim[0]=1;
+	nim[1]=2;
+	nim[2]=3;
+	nim[3]=4;
+	nim[4]=5;
+	nim[5]=6;
+	nim[6]=7;
+	
+	printf("Data ke-1 = %d\n",nim[0]);
+	printf("Data ke-2 = %d\n",nim[1]);
+	printf("Data ke-3 = %d\n",nim[2]);
+	printf("Data ke-4 = %d\n",nim[3]);
+	printf("Data ke-5 = %d\n",nim[4]);
+	printf("Data ke-6 = %d\n",nim[5]);
+	printf("Data ke-7 = %d\n",nim[7]);
+}
