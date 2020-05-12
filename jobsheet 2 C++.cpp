66516319a03c3cd10919f5544258cd1e3af9dd83
Lalu Ferdian Yusuf <64566098+laluferdianyusuf@users.noreply.github.com>
2020-05-12 17:13:13 +0800

    Create jobsheet 2 C++.cpp
    Mengisi dan menampilkan elemen array index berdasarkan NIM

---
+++ jobsheet 2 C++.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+	cout << "Nama     : Lalu Ferdian Yusuf"<<endl;
+	cout << "Nim      : F1B019077"<<endl;
+	cout << "Kelompok : 16"<<endl;
+	
+	int x [7];
+	
+	cout << "masukan nilai ke-1: ";
+	cin >>x[0];
+	
+	cout << "masukan nilai ke-2: ";
+	cin >>x[1];
+	
+	cout << "masukan nilai ke-3: ";
+	cin >>x[2];
+	
+	cout << "masukan nilai ke-4: ";
+	cin >>x[3];
+	
+	cout << "masukan nilai ke-5: ";
+	cin >>x[4];
+	
+	cout << "masukan nilai ke-6: ";
+	cin >>x[5];
+	
+	cout << "masukan nilai ke-7: ";
+	cin >>x[6];
+}
