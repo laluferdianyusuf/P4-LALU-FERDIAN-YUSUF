13b74243560c898d202e868792885c430e0324a4
Lalu Ferdian Yusuf <64566098+laluferdianyusuf@users.noreply.github.com>
2020-05-12 17:08:05 +0800

    Create jobsheet 1 C++.cpp
    Menampilkan array satu dimensi

---
+++ jobsheet 1 C++.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+	cout << "Nama     : Lalu Ferdian Yusuf"<<endl;
+	cout << "Nim      : F1B019077"<<endl;
+	cout << "Kelompok : 16"<<endl;
+	
+int NIM_Mhs [7] = {1,2,5,4,6,19,10};
+
+cout << ("data ke-1 =",NIM_Mhs [0])<<endl;
+cout << ("data ke-2 =",NIM_Mhs [1])<<endl;
+cout << ("data ke-3 =",NIM_Mhs [2])<<endl;
+cout << ("data ke-4 =",NIM_Mhs [3])<<endl;
+cout << ("data ke-5 =",NIM_Mhs [4])<<endl;
+cout << ("data ke-6 =",NIM_Mhs [5])<<endl;
+cout << ("data ke-7 =",NIM_Mhs [6])<<endl;
+
+}
